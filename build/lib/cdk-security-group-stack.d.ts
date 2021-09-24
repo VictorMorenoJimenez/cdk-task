87672185849586c5b5133ea8596e442a31ea155c
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-24 11:17:50 +0200

    Delete build

--- build/lib/cdk-security-group-stack.d.ts
+++
@@ -1,9 +0,0 @@
-import * as cdk from '@aws-cdk/core';
-export interface CdkSecurityGroupStackProps extends cdk.StackProps {
-    ipToAllow: string;
-    securityGroupId: string;
-}
-export declare class CdkSecurityGroupStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props: CdkSecurityGroupStackProps);
-    private getSecurityGroup;
-}
